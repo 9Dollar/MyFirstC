e683dd1bcf1a38bd6adc32ce74d67eebd441eca3
9Dollar <hanseungyun06@gmail.com>
2022-01-08 10:42:35 +0900

    찐처음푸시

---
+++ printfAndscanf/printfAndscanf/printfscanf.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+
+	//실수변수 float
+	/*
+	float f = 46.5f;
+	printf("%.1f\n", f);
+	*/
+
+	//실수변수 double
+	/*
+	double d = 4.428;
+	printf("%.3lf\n", d);
+	*/
+
+	//땅뚜(상수)
+	//const int = 2000;
+
+	//scanf
+	//키보드 입력받고 저장
+	/*int input;
+	printf("값을 입력하세요 : ");
+	scanf_s("%d", &input);
+	printf("\n값은 %d", input);*/
+
+	//문자
+	/*char c = 'A';
+	printf("%c\n", c);*/
+
+	//문자열
+	/*char str[256];
+	scanf_s("%s", str, sizeof(str));
+	printf("%s\n", str);*/
+
+	return 0;
+}
